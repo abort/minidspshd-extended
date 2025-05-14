0a92671a6865b78df41871cc98d1ab74590e33b2
Jorryt Dijkstra <jdijkstra@Jorryts-MBP.home.arpa>
2025-05-14 22:21:11 +0200

    update

--- custom_components/minidspshd/media_player.py
+++ custom_components/minidspshd/media_player.py
@@ -153,7 +153,8 @@ class Volumio(MediaPlayerEntity, RestoreEntity):
 
     def _on_minidsp_update(self, data: MiniDSPState) -> None:
         _LOGGER.info(f"received minidsp update: {data}")
-        self._attr_sound_mode = f"Preset {int(data.preset) + 1} (Dirac: {"ON" if data.dirac else "OFF"})"
+        s = "ON" if data.dirac else "OFF"
+        self._attr_sound_mode = f"Preset {int(data.preset) + 1} (Dirac: {s})"
         self._attr_is_volume_muted = data.mute
         self._state["mute"] = True
         self._is_available = True
